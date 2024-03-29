00ef4374458ba9747584c2421fc2133497ac888b
Raihan <17radf@gmail.com>
2024-03-29 16:46:49 +0700

    feat: add dark mode switcher

--- app/src/main/java/com/raihan/githubapp/ui/fragment/setting_page/SettingFragment.kt
+++ app/src/main/java/com/raihan/githubapp/ui/fragment/setting_page/SettingFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.raihan.githubapp.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
+
 	private var _binding: FragmentSettingBinding? = null
 	private val b get() = _binding!!
 
+	private val themeOptions = listOf("Light", "Dark", "System")
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
@@ -28,5 +32,26 @@ class SettingFragment : Fragment() {
 				activity?.onBackPressedDispatcher?.onBackPressed()
 			}
 		}
+
+		b.themeSettingButton.setOnClickListener {
+			showThemeDialog()
+		}
 	}
+
+	private fun showThemeDialog() {
+		val builder = MaterialAlertDialogBuilder(requireActivity())
+			.setTitle("Theme Preference")
+			.setSingleChoiceItems(themeOptions.toTypedArray(), 2) { dialog, which ->
+				val selectedTheme = when (which) {
+					0 -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+					1 -> AppCompatDelegate.MODE_NIGHT_NO
+					else -> AppCompatDelegate.MODE_NIGHT_YES
+				}
+				dialog.dismiss()
+				AppCompatDelegate.setDefaultNightMode(selectedTheme)
+			}
+			.create()
+		builder.show()
+	}
+
 }
